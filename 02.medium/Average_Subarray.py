527559fd3df332b3de1fd8738ca674fbead3bd8a
Eric Mencarini <ericmencarini@outlook.com>
2025-09-25 21:12:18 -0300

    add_2_medium

---
+++ 02.medium/Average_Subarray.py
@@ -0,0 +1,6 @@
+#https://datalemur.com/questions/python-average-subarray
+
+
+nums = [1, 2, -5, -3, 10, 3]
+k = 3
+print(sorted(nums))
--- 02.medium/Min_Amplitude.py
+++ 02.medium/Min_Amplitude.py
@@ -2,5 +2,24 @@
 
 arr = [4, -8, -1, 3, 7, 10, 5]
 
+
 def min_amplitude(arr):
-	return -1
+	
+	arr.sort()
+	
+	n = len(arr)
+
+	if n <= 4:
+		return 0
+	arr1 = arr[n-1] - arr[3]
+	print(arr1)
+	arr2 = arr[n-2] - arr[2]
+	print(arr2)
+	arr3 = arr[n-3] - arr[1]
+	print(arr3)
+	arr4 = arr[n-4] - arr[0]
+	print(arr4)
+
+	return min(arr1,arr2,arr3,arr4)
+
+print(min_amplitude(arr))
