5ae20ab1d05b1ba1f9480cc76ac1779bb3140ea7
Eric Mencarini <ericmencarini@outlook.com>
2025-09-29 20:43:12 -0300

    add_2_medium_

---
+++ 02.medium/Hill_Climbing.py
@@ -0,0 +1,4 @@
+#https://datalemur.com/questions/python-hill-climbing
+
+def climb_hill(n):
+	return -1
--- 02.medium/Idle_GPU_Days.py
+++ 02.medium/Idle_GPU_Days.py
@@ -1,4 +1,12 @@
 #https://datalemur.com/questions/python-idle-gpu-days
 
+training_sessions = [[2, 4], [1, 5], [7, 8]] 
+days = 9
+
 def gpu_idle_days(days, training_sessions):
-	return -1
+	ts_fill = [list(range(start, end+1)) for start, end in training_sessions]
+	flat = [x for z in ts_fill for x in z]
+	ordered_flat = sorted(set(flat))
+	return days - len(ordered_flat)
+
+print(gpu_idle_days(days, training_sessions))
