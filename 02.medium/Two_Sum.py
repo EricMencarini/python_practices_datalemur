a749ef221d10c422befd65415928c8999b7f5122
Eric Mencarini <ericmencarini@outlook.com>
2025-10-17 07:18:24 -0300

    add_1_medium

--- 02.medium/Two_Sum.py
+++ 02.medium/Two_Sum.py
@@ -8,7 +8,18 @@ If there is no valid solution, return [-1, -1].
 Return the indices in increasing order (i.e. [1,3], NOT [3,1]).
  nums = [1, 4, 6, 10], target = 10
 '''
+nums = [1, 4, 6, 10] 
+target = 10
 
 def two_sum(nums: list[int], target: int) -> list[int]:
-    return [-1, -1]
-    
+    i = 0
+    j = 1
+    while i < len(nums):
+        while j < len(nums):
+            if nums[i] + nums[j] == target:
+                return sorted([i,j])
+            j += 1
+        i += 1
+        j = i + 1
+
+print(two_sum(nums, target))
